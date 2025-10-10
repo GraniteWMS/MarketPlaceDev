d660bb469094c6f3473b5f7a51705d5389d0c392
JoshMeijer <36229349+JoshMeijer@users.noreply.github.com>
2025-10-10 10:04:07 +0200

    NEW - Test Publishing 2 artifacts: SQL_STORED_PROCEDURE/PrescriptTest, PROCESS/ADJUSTMENT

---
+++ PROCESS/3d0bde5d-08b3-4663-b1d0-084b791e2208/v1/ADJUSTMENT.sql
@@ -0,0 +1,58 @@
+-- =============================================
+-- Process Template Deployment Script
+-- Generated: 2025-10-10 10:03:59
+-- =============================================
+
+-- =============================================
+-- DATA INSERTS
+-- =============================================
+
+-- ProcessData
+-- =============================================
+-- Process Data Inserts
+-- =============================================
+
+-- Process
+INSERT INTO [Process] ([isActive],[Name],[Description],[Type],[IntegrationMethod],[IntegrationIsActive],[IntegrationPost],[WebTemplate])
+VALUES('True','ADJUSTMENT','ADJUSTMENT','ADJUSTMENT','','False','False','')
+
+-- ProcessSteps
+INSERT INTO [ProcessStep] ([Name],[Description],[ProcessIndex],[isActive],[Required],[DefaultValue],[NextIndex],[Process],[PreScript],[WebTemplate])
+VALUES('Reference','Reference',0,'True','False','',1,'ADJUSTMENT','','')
+
+INSERT INTO [ProcessStep] ([Name],[Description],[ProcessIndex],[isActive],[Required],[DefaultValue],[NextIndex],[Process],[PreScript],[WebTemplate])
+VALUES('TrackingEntity','TrackingEntity',1,'True','True','',2,'ADJUSTMENT','PrescriptTest','')
+
+INSERT INTO [ProcessStep] ([Name],[Description],[ProcessIndex],[isActive],[Required],[DefaultValue],[NextIndex],[Process],[PreScript],[WebTemplate])
+VALUES('Type','Type',2,'True','True','',3,'ADJUSTMENT','','')
+
+INSERT INTO [ProcessStep] ([Name],[Description],[ProcessIndex],[isActive],[Required],[DefaultValue],[NextIndex],[Process],[PreScript],[WebTemplate])
+VALUES('Qty','Qty',3,'True','True','',4,'ADJUSTMENT','','')
+
+INSERT INTO [ProcessStep] ([Name],[Description],[ProcessIndex],[isActive],[Required],[DefaultValue],[NextIndex],[Process],[PreScript],[WebTemplate])
+VALUES('Comment','Comment',4,'True','False','',5,'ADJUSTMENT','','')
+
+INSERT INTO [ProcessStep] ([Name],[Description],[ProcessIndex],[isActive],[Required],[DefaultValue],[NextIndex],[Process],[PreScript],[WebTemplate])
+VALUES('Confirmation','Confirmation',5,'True','False','',1,'ADJUSTMENT','','')
+
+-- ProcessStepLookups
+INSERT INTO [ProcessStepLookup] ([Value],[Description],[Process],[ProcessStep])
+VALUES('CORRECT TO PHYSICAL','CORRECT TO PHYSICAL','ADJUSTMENT','Comment')
+
+INSERT INTO [ProcessStepLookup] ([Value],[Description],[Process],[ProcessStep])
+VALUES('DAMAGED STOCK','DAMAGED STOCK','ADJUSTMENT','Comment')
+
+INSERT INTO [ProcessStepLookup] ([Value],[Description],[Process],[ProcessStep])
+VALUES('EXPIRED STOCK','EXPIRED STOCK','ADJUSTMENT','Comment')
+
+INSERT INTO [ProcessStepLookup] ([Value],[Description],[Process],[ProcessStep])
+VALUES('INCORRECT LABEL','INCORRECT LABEL','ADJUSTMENT','Comment')
+
+INSERT INTO [ProcessStepLookup] ([Value],[Description],[Process],[ProcessStep])
+VALUES('PROCESS ERROR','PROCESS ERROR','ADJUSTMENT','Comment')
+
+
+
+-- =============================================
+-- END OF SCRIPT
+-- =============================================
