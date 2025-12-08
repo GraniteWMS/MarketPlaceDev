725aa90e7b64c8ae100a5d675f0bb9ba398bf6b4
ParAg0n95 <66292163+ParAg0n95@users.noreply.github.com>
2025-12-08 08:04:36 +0200

    Update USER_TABLE: TestTable20251208

--- ParAg0n95/USER_TABLE/testtable20251208/latest/TestTable20251208.sql
+++ ParAg0n95/USER_TABLE/testtable20251208/latest/TestTable20251208.sql
@@ -12,8 +12,6 @@ CREATE TABLE [dbo].[TestTable20251208](
 	[OnHold] [bit] NOT NULL,
 	[StockTake] [bit] NOT NULL,
 	[InStock] [bit] NOT NULL,
-	[MasterItem_id] [bigint] NOT NULL,
-	[Location_id] [bigint] NOT NULL,
 PRIMARY KEY CLUSTERED 
 (
 	[ID] ASC
