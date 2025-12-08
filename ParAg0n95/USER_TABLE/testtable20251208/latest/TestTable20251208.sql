acaa74de9048c2d857a9d37d816c859a9f1b3a03
ParAg0n95 <66292163+ParAg0n95@users.noreply.github.com>
2025-12-08 08:29:46 +0200

    Update USER_TABLE: TestTable20251208

--- ParAg0n95/USER_TABLE/testtable20251208/latest/TestTable20251208.sql
+++ ParAg0n95/USER_TABLE/testtable20251208/latest/TestTable20251208.sql
@@ -10,7 +10,6 @@ CREATE TABLE [dbo].[TestTable20251208](
 	[Batch] [varchar](50) COLLATE Latin1_General_CI_AS NULL,
 	[ExpiryDate] [datetime] NULL,
 	[OnHold] [bit] NOT NULL,
-	[StockTake] [bit] NOT NULL,
 PRIMARY KEY CLUSTERED 
 (
 	[ID] ASC
