9b28bc57b8e6e75a7b9673cc8a32aa9184e5a4f8
ParAg0n95 <66292163+ParAg0n95@users.noreply.github.com>
2026-01-22 14:06:56 +0200

    Publish 1 artifact(s)

--- JoshMeijer/USER_TABLE/customtable/latest/CustomTable.sql
+++ JoshMeijer/USER_TABLE/customtable/latest/CustomTable.sql
@@ -5,6 +5,8 @@ CREATE TABLE [dbo].[CustomTable](
 	[Date] [datetime] NOT NULL,
 	[Document_id] [bigint] NOT NULL,
 	[DocumentLine_id] [bigint] NOT NULL,
-	[TrackingEntity_id] [bigint] NOT NULL
+	[TrackingEntity_id] [bigint] NOT NULL,
+	[Test] [nchar](10) COLLATE Latin1_General_CI_AS NULL,
+	[Test32] [nchar](10) COLLATE Latin1_General_CI_AS NULL
 ) ON [PRIMARY]
 
