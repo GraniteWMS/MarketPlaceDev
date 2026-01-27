48baf3b5648a22164f2d00bd647d07d120d9173d
JoshMeijer <36229349+JoshMeijer@users.noreply.github.com>
2026-01-27 09:16:50 +0200

    Publish 3 artifact(s)

---
+++ JoshMeijer/DATA/cage-receiving/v1/CAGE_RECEIVING.sql
@@ -0,0 +1,27 @@
+-- =============================================
+-- Process Data Inserts
+-- =============================================
+
+-- Process
+INSERT INTO [Process] ([isActive],[Name],[Description],[Type],[IntegrationMethod],[IntegrationIsActive],[IntegrationPost],[WebTemplate])
+VALUES('True','CAGE_RECEIVING','CAGE_RECEIVING','RECEIVING','','False','False','')
+
+-- ProcessSteps
+INSERT INTO [ProcessStep] ([Name],[Description],[ProcessIndex],[isActive],[Required],[DefaultValue],[NextIndex],[Process],[PreScript],[WebTemplate])
+VALUES('Location','Receiving Location',0,'True','False','',1,'CAGE_RECEIVING','','')
+
+INSERT INTO [ProcessStep] ([Name],[Description],[ProcessIndex],[isActive],[Required],[DefaultValue],[NextIndex],[Process],[PreScript],[WebTemplate])
+VALUES('Document','Document',1,'True','True','',2,'CAGE_RECEIVING','','{{ "SELECT Document, Supplier FROM WebTemplate_CageReceiving_Document WHERE Cage = @Location" | dbSelect({Location}) | basicTable({selectOnColumn:"Document", enableSearch:true}) }}')
+
+INSERT INTO [ProcessStep] ([Name],[Description],[ProcessIndex],[isActive],[Required],[DefaultValue],[NextIndex],[Process],[PreScript],[WebTemplate])
+VALUES('MasterItem','MasterItem',2,'True','True','',3,'CAGE_RECEIVING','','{{ "SELECT Code, Description, Completion FROM WebTemplate_CageReceiving_MasterItem WHERE Document = @Document" | dbSelect({Document}) | basicTable({selectOnColumn:"Code", enableSearch:true}) }}')
+
+INSERT INTO [ProcessStep] ([Name],[Description],[ProcessIndex],[isActive],[Required],[DefaultValue],[NextIndex],[Process],[PreScript],[WebTemplate])
+VALUES('Qty','Qty',3,'True','True','',4,'CAGE_RECEIVING','','')
+
+INSERT INTO [ProcessStep] ([Name],[Description],[ProcessIndex],[isActive],[Required],[DefaultValue],[NextIndex],[Process],[PreScript],[WebTemplate])
+VALUES('NoEntities','Number of TrackingEntities',4,'True','False','',2,'CAGE_RECEIVING','','')
+
+INSERT INTO [ProcessStep] ([Name],[Description],[ProcessIndex],[isActive],[Required],[DefaultValue],[NextIndex],[Process],[PreScript],[WebTemplate])
+VALUES('Print','Print',99,'True','True','',0,'CAGE_RECEIVING','','')
+
