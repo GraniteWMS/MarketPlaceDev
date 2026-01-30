6ad1ec33a0a3a2eb377545e17a3338170a99fdb9
JoshMeijer <36229349+JoshMeijer@users.noreply.github.com>
2026-01-30 11:27:59 +0200

    Publish 5 artifact(s)

---
+++ JoshMeijer/DATA/cage-receiving-1/v1/CAGE_RECEIVING.sql
@@ -0,0 +1,27 @@
+-- =============================================
+-- Process Data Inserts
+-- =============================================
+
+-- Process
+INSERT INTO [Process] ([isActive],[Name],[Description],[Type],[IntegrationMethod],[IntegrationIsActive],[IntegrationPost],[WebTemplate])
+VALUES('True','CAGE_RECEIVING','CAGE_RECEIVING','RECEIVING','','False','False',NULL)
+
+-- ProcessSteps
+INSERT INTO [ProcessStep] ([Name],[Description],[ProcessIndex],[isActive],[Required],[DefaultValue],[NextIndex],[Process],[PreScript],[WebTemplate])
+VALUES('Location','Receiving Location',0,'True','False','',1,'CAGE_RECEIVING','',NULL)
+
+INSERT INTO [ProcessStep] ([Name],[Description],[ProcessIndex],[isActive],[Required],[DefaultValue],[NextIndex],[Process],[PreScript],[WebTemplate])
+VALUES('Document','Document',1,'True','True','',2,'CAGE_RECEIVING','','544f79e8-c867-4f31-b056-18cc7cfc52a7')
+
+INSERT INTO [ProcessStep] ([Name],[Description],[ProcessIndex],[isActive],[Required],[DefaultValue],[NextIndex],[Process],[PreScript],[WebTemplate])
+VALUES('MasterItem','MasterItem',2,'True','True','',3,'CAGE_RECEIVING','','2bb651f8-97a2-4239-bf07-d50c8fe1fc1f')
+
+INSERT INTO [ProcessStep] ([Name],[Description],[ProcessIndex],[isActive],[Required],[DefaultValue],[NextIndex],[Process],[PreScript],[WebTemplate])
+VALUES('Qty','Qty',3,'True','True','',4,'CAGE_RECEIVING','',NULL)
+
+INSERT INTO [ProcessStep] ([Name],[Description],[ProcessIndex],[isActive],[Required],[DefaultValue],[NextIndex],[Process],[PreScript],[WebTemplate])
+VALUES('NoEntities','Number of TrackingEntities',4,'True','False','',2,'CAGE_RECEIVING','',NULL)
+
+INSERT INTO [ProcessStep] ([Name],[Description],[ProcessIndex],[isActive],[Required],[DefaultValue],[NextIndex],[Process],[PreScript],[WebTemplate])
+VALUES('Print','Print',99,'True','True','',0,'CAGE_RECEIVING','',NULL)
+
