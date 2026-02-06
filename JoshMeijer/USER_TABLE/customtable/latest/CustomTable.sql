5244ad54e6bdb89e06f1406f205a3e7f75bde549
ParAg0n95 <66292163+ParAg0n95@users.noreply.github.com>
2026-02-06 14:21:57 +0200

    Update: CustomTable

--- JoshMeijer/USER_TABLE/customtable/latest/CustomTable.sql
+++ JoshMeijer/USER_TABLE/customtable/latest/CustomTable.sql
@@ -4,6 +4,7 @@ CREATE TABLE [dbo].[CustomTable](
 	[ID] [bigint] IDENTITY(1,1) NOT NULL,
 	[Document_id] [bigint] NOT NULL,
 	[DocumentLine_id] [bigint] NOT NULL,
-	[TEST] [nchar](10) COLLATE Latin1_General_CI_AS NULL
+	[TEST] [nchar](10) COLLATE Latin1_General_CI_AS NULL,
+	[TTest2] [nchar](10) COLLATE Latin1_General_CI_AS NULL
 ) ON [PRIMARY]
 
