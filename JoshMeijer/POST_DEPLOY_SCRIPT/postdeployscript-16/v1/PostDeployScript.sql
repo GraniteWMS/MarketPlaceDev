6fb7e4c04a236a2fddcc3bd9de6bb57d15c1cc89
JoshMeijer <36229349+JoshMeijer@users.noreply.github.com>
2026-03-10 11:02:14 +0200

    Delete JoshMeijer directory

--- JoshMeijer/POST_DEPLOY_SCRIPT/postdeployscript-16/v1/PostDeployScript.sql
+++
@@ -1,4 +0,0 @@
-IF NOT EXISTS(SELECT ID FROM [Location] WHERE Barcode = 'SCRAP' OR [Name] = 'SCRAP')
- BEGIN
- INSERT INTO [Location] (Barcode, [Name], isActive) VALUES ('SCRAP', 'SCRAP', 1)
- END
