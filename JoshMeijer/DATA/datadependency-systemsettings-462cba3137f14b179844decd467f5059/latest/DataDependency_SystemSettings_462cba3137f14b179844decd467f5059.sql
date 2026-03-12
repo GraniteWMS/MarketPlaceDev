6d73fc6ef9ad4d02451b996c658c83157b10a44a
Josh Meijer <joshm@cradle.co.za>
2026-03-12 14:00:17 +0200

    Remove all artifacts

--- JoshMeijer/DATA/datadependency-systemsettings-462cba3137f14b179844decd467f5059/latest/DataDependency_SystemSettings_462cba3137f14b179844decd467f5059.sql
+++
@@ -1,6 +0,0 @@
--- =============================================
--- Data Dependency Inserts
--- =============================================
--- dbo.SystemSettings
-
-INSERT INTO [dbo].[SystemSettings] ([Application], [Key], [Value], [Description], [ValueDataType], [isActive], [isEncrypted], [EncryptionKey], [AuditDate], [AuditUser], [Version], [GUID]) VALUES (N'Granite.Custodian', N'Token', N'qlL0OC2DjetOxQWFfaVHomADvFcjHqJsmRwCDly5SjA8rwj6tNxEN4KqwWlma3tGpDP3TZHkzUH+AzcYGdxMs4MQoZ8nGsuqewxijDVbTp79II816l4bEFfsLVHvWp7Rr3MEEo4U7n1MDCCwqbPdFjo9ixo2A5SFMUZD1/+UGcd2DuFcegJ5+9nJYV2za0Own7ZsfuPL4WipDjS/2oBhkU7w36HBOVNOuaG/O6bMQo6Uya2C3lGX5VPalYsd8Uqw', N'', N'string', 1, 1, N'3SsPIaHkYgQdiMhgeRsMpA==', '2026-01-20 13:54:40.537', N'MANUAL', 6, '7a49150b-0005-4c07-917b-50a2db7afce9');
