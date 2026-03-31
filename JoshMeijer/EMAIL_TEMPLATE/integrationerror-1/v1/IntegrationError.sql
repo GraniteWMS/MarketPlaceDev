f5e41dadecfb54845860f603f280fb7f7217079b
JoshMeijer <36229349+JoshMeijer@users.noreply.github.com>
2026-03-31 15:00:48 +0200

    Publish: IntegrationError

---
+++ JoshMeijer/EMAIL_TEMPLATE/integrationerror-1/v1/IntegrationError.sql
@@ -0,0 +1,25 @@
+-- =============================================
+-- Email Template Data Insert
+-- =============================================
+SET NOCOUNT ON;
+
+    INSERT INTO [EmailTemplate] ([Name], [Description], [Definition], [Format], [AuditDate], [AuditUser], [Version])
+    VALUES (N'IntegrationError', N'This template is used by Scheduler injected jobs to notify of a failed document sync', N'{{
+  headerImage({
+    imageUrl:''https://www.granitewms.com/wp-content/uploads/2020/07/GraniteWMS-1.png'',
+    backgroundColor:''#182026''
+  })
+}}
+
+# Document {{documentNumber}} failed to sync
+
+{{#if documentLogs}}
+### Document header failed to sync:
+{{documentLogs}}
+{{/if}}
+
+{{#if documentDetailLogs}}
+### Document details failed to sync:
+{{documentDetailLogs}}
+{{/if}}', N'markdown', GETDATE(), N'AUTOMATION', 1);
+
